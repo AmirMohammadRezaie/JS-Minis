5d32cd333cb395f014f80265820830706083c19f
Amir <amirrezaie.dev@gmail.com>
2024-08-11 18:56:42 +0330

    feat(ui): add random color functionality to Color Flipper

--- 1/script.js
+++ 1/script.js
@@ -7,3 +7,12 @@ function setColor(name) {
 
 setColor('green')
 
+
+function randomColor(){
+    const red = Math.round(Math.random() * 255)
+    const green = Math.round(Math.random() * 255)
+    const blue = Math.round(Math.random() * 255)
+
+    const color = `rgb(${red}, ${green}, ${blue})`
+    body.style.backgroundColor = color
+}
